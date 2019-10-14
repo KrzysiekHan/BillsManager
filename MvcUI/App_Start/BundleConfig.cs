b1ed86a7fad6900a400eaa053d1d2d32e35a0df9
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2019-10-14 16:25:52 +0200

    toastr

--- MvcUI/App_Start/BundleConfig.cs
+++ MvcUI/App_Start/BundleConfig.cs
@@ -14,12 +14,15 @@ namespace MvcUI
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*",
                         "~/Scripts/jquery.validate.unobtrusive.min.js"));
-
+            
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/thirdParty").Include(
+                      "~/lib/toastr.js/toastr.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
