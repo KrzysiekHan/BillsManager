b6d170629022f71067ff92b44e0816cef5e29874
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2019-10-18 22:39:51 +0200

    stacked chart

---
+++ MvcUI/Models/Home/ChartDataSet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcUI.Models.Home
+{
+    public class ChartDataSet
+    {
+        public string Label { get; set; }
+        public List<int> Data { get; set; }
+    }
+}
