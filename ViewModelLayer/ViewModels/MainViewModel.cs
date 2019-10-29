5ce6b343d697bf87acfc2efa15c043e57ad769d6
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2019-10-29 14:38:08 +0100

    file restored

---
+++ ViewModelLayer/ViewModels/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViewModelLayer.Models;
+
+namespace ViewModelLayer.ViewModels
+{
+    public class MainViewModel
+    {
+        List<Bill> Bills { get; set; }
+        List<Recipient> Recipients { get; set; }
+        List<BillType> Types { get; set; }
+    }
+}
